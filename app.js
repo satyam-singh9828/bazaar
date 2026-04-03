31bfa69df5f3277a5e0e92a2c7b82e96ca7c91ae
kidoos.010 <145568555+satyam-singh9828@users.noreply.github.com>
2026-04-03 11:39:23 +0530

    update

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://your-frontend.vercel.app"
+      "https://apnabazzarr.netlify.app"
     ],
     methods: ["GET", "POST"],
     credentials: true
